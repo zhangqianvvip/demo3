2eb1b26e5c5e79db63a9f300ca400aa0b5281e45
zq <zhangqian_vvip@163.com>
2019-03-25 09:11:36 +0800

    qwe

--- workspace-sts-3.9.3.RELEASE/qwe/src/main/java/com/mian/controller/Bean.java
+++ workspace-sts-3.9.3.RELEASE/qwe/src/main/java/com/mian/controller/Bean.java
@@ -1,16 +1,18 @@
 package com.mian.controller;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Bean {
 
 	public static void main(String[] args) {
-		AppliactionContext context = new ClassPathXmlApplicationContext("classpath:/META-INF\"/applaction.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/META-INF/applaction.xml");
 		int count = context.getBeanDefinitionCount();
 		System.out.println("Bean的个数"+count);
 		String[] names=context.getBeanDefinitionNames();
 		for(String name :names) {
-			System.out.println();
+			System.out.println(name);
+			System.out.println(context.containsBeanDefinition(name));
 		}
 	}
 
