8e3ad3b77f0dd2084eff4fe01a8e0ca2eaaa9e70
zq <zhangqian_vvip@163.com>
2019-03-25 11:13:18 +0800

    demo

--- workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/controller/controller.java
+++ workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/controller/controller.java
@@ -1,13 +1,17 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
+	@Value("${name}")
+	private String name;
+	
 	@RequestMapping("/hello")
 	public String index() {
-		return "helloworld";
+		return "helloworld,"+name;
 				
 	}
 }
