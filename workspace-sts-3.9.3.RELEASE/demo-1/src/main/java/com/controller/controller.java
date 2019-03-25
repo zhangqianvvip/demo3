35fdd1539fe9eb3f8d1ec942b22819d63b95f37e
zq <zhangqian_vvip@163.com>
2019-03-25 10:51:29 +0800

    qwe

---
+++ workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/controller/controller.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	@RequestMapping("/hello")
+	public String index() {
+		return "helloworld";
+				
+	}
+}
