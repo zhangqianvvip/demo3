aaae07429943a61616ed6ebb76bec5a19bbeaba3
zq <zhangqian_vvip@163.com>
2019-03-26 09:14:08 +0800

    demo

--- workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/controller/controller.java
+++ workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/controller/controller.java
@@ -1,17 +1,44 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.bean.ConfigBean;
+
 @RestController
+//@EnableConfigrationProperties({ConfigBean.class})
+@Component
+@Profile({"prod"})
 public class controller {
-	@Value("${name}")
+	@Autowired
+	ConfigBean configBean;
+	
+	@RequestMapping(value = "/config")
+	public String config() {
+		return configBean.toString();
+	}
+	
+	@Value("${hello:name}")
+	private String hello;
+	
+	@Value("${name:age}")
 	private String name;
 	
-	@RequestMapping("/hello")
-	public String index() {
-		return "helloworld,"+name;
-				
+	@RequestMapping(value = "/my")
+	public String my() {
+		return name;
+	}
+	@RequestMapping("hellow")
+	public String hellow() {
+		return "";
+	}
+	@RequestMapping("hellow1")
+	public String getMessage() {
+		return this.hello + "" + this.name;
 	}
 }
+	
--- workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ workspace-sts-3.9.3.RELEASE/demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -1,15 +1,56 @@
 package com.example.demo;
 
+import java.util.stream.Collectors;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 @SpringBootApplication
-@ComponentScan("com.controller")
+@ComponentScan({"com.controller","com.example.bean"})
 public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("list beans:"+ctx.getBeanDefinitionCount());
+		};
+	}
+	
+	@Bean
+	public ApplicationRunner appRunner() {
+		return args ->{
+			System.out.println();
+			System.out.println("Application Runner:");
+			for(String opt : args.getOptionNames()) {
+				System.out.println(opt);
+				System.out.println("->");
+				System.out.println(args.getOptionValues(opt).stream().collect(Collectors.joining(",","[","]")));
+			}
+		};
+	}
+	
+	@Bean
+	public ApplicationListener<ApplicationEvent> helloListener(){
+		final String HELLO_URL = "/hello";
+		return (ApplicationEvent event) ->{
+			if(event instanceof ServletRequestHandledEvent) {
+				ServletRequestHandledEvent e = (ServletRequestHandledEvent) event;
+				if(e.getRequestUrl().equals(HELLO_URL)) {
+					System.out.println("visit hello");
+				}
+			}
+		};
+	}
+	
 }
